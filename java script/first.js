ba1ff1faafef0db898404768278bdca808238a7a
Md.TauhidIslam <168570535+tawhid-11@users.noreply.github.com>
2025-11-26 13:13:56 +0600

    Create first.js

---
+++ java script/first.js
@@ -0,0 +1,14 @@
+alert('Hi javascript');
+document.getElementById('title').innerHTML = 'Hello World';
+
+let sum= 50+50;
+console.log(sum);
+
+if(sum>100){
+  console.log('Expensive');
+}
+else{
+  console.log('chip');
+}
+
+

fdd083d9c5564c5de2911e70f28888f6687cb6d7
Md.TauhidIslam <168570535+tawhid-11@users.noreply.github.com>
2025-12-01 15:02:26 +0600

    Create script.js

---
+++ java script/script.js
@@ -0,0 +1,16 @@
+document.querySelector('h1').innerText='Subscribe';
+
+const box= document.querySelector('.box');
+box.style.backgroundColor='red';
+box.style.width='100px';
+box.style.height='100px';
+
+
+document.getElementById('box').style.backgroundColor='blue';
+
+const menuItem= document.querySelectorAll('a');
+menuItem[1].style.color='green';
+menuItem.forEach(function(i){
+  i.style.color= 'red';
+  i.style.fontsize='24px';
+});
